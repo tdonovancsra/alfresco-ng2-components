af1deef29e5d71ec687b7e6e2499dcdd28b69b8b
mauriziovitale84 <maurizio.vitale@alfresco.com>
2017-07-06 11:15:09 +0100

    use the same upload component between the content and process service

--- ng2-components/ng2-activiti-tasklist/index.ts
+++ ng2-components/ng2-activiti-tasklist/index.ts
@@ -16,7 +16,7 @@
  */
 
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { MdIconModule, MdButtonModule } from '@angular/material';
+import { MdIconModule, MdButtonModule, MdProgressSpinnerModule } from '@angular/material';
 import { CoreModule } from 'ng2-alfresco-core';
 import { DataTableModule } from 'ng2-alfresco-datatable';
 import { ActivitiFormModule } from 'ng2-activiti-form';
@@ -73,7 +73,8 @@ export const ACTIVITI_TASKLIST_PROVIDERS: any[] = [
         DataTableModule,
         ActivitiFormModule,
         MdIconModule,
-        MdButtonModule
+        MdButtonModule,
+        MdProgressSpinnerModule
     ],
     declarations: [
         ...ACTIVITI_TASKLIST_DIRECTIVES
